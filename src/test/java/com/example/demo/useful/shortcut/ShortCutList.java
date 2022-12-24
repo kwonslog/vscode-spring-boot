6764320d56bdec124d4c1a9b7d0265b7abe6a145
kwons <korpgm@gmail.com>
2022-12-24 23:45:26 +0900

    vs code 단축키 정리

---
+++ src/test/java/com/example/demo/useful/shortcut/ShortCutList.java
@@ -0,0 +1,49 @@
+package com.example.demo.useful.shortcut;
+
+public class ShortCutList {
+
+  public static void main(String[] args) {
+    System.out.println("vscode 쓸만한 단축키 모음");
+
+    // 우선순위로 정리
+
+    // 마지막수정위치이동 ctrl + k > ctrl + q
+    // 오류위치이동 f8
+    // 리팩토링 ctrl + shift + r
+    // 함수정의이동 ctrl + 클릭, f12
+    // 함수정의간략보기 alt + f12
+    // 다음찾기 f3
+    // 저장하기전에변경사항비교 ctrl + k + d
+    
+    // 실행 ctrl + f5
+
+    // 설정창 ctrl + ,
+    // 터미널창토클 ctrl + `
+    // 탐색기 ctrl + b
+    // 폰트크기 ctrl + '+', ctrl + '-'
+    // VScode멀티 ctrl + shift + n
+    
+    // 화면분할 ctrl + \   
+    // 전체파일검색 ctrl + shift + f
+    // 전체파일변경 ctrl + shift + h
+    // 소스정렬 shift + alt + f
+    // 소스라인이동 ctrl + g
+    // 한줄복사 alt + shift + up/down
+    // 한줄이동 alt + up/down
+    // 한줄삭제 shift + delete
+    // 같은단어하나씩선택 ctrl + d
+    // 같은단어전체선택 ctrl + shift + l
+    // 같은단어한번에변경 f2
+    // 멀티커서 alt + 클릭
+    // 멀티입력 ctrl + alt + up/down
+    // 영역선택 shift + alt + 드래그
+    // 파일열기 ctrl + p
+    // 주석 ctrl + /, alt + shift + a
+    // 좌우분활화면이동 ctrl + 1, ctrl + 2
+    // 영역선택한줄끝에멀티커서 shift + alt + i
+    // 열려있는페이지이동 ctrl + pgup, ctrl + pgdn
+    // 들여쓰기 ctrl + ], ctrl + [
+    // 들여쓰기간격 ctrl + shift + p, Indent Using Spaces
+
+  }
+}
